ae0966652465f1624a2275ffa40abb849f549a69
dev-harsh1998 <dev-harsh1998@hotmail.com>
2019-07-11 22:49:39 +0530

    devupload: add simple methhod to parse credentials from stdin

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,25 @@ extern crate clap;
 use std::io;
 use clap::{Arg, App};
 
+struct Credentials <'a> {
+    username: &'a str,
+    pass: &'a str
+}
+
+fn get_credentials() {
+    let mut u_details: Credentials = Credentials {username: "PLACEHOLDER", pass: "PLACEHOLDER"};
+    println!("Enter your username below: ");
+    let mut temp: String = String::new();
+    io::stdin().read_line(&mut temp).expect("No Input!");
+    temp = temp.trim().to_string();
+    u_details.username = &temp;
+    println!("Enter your username below: ");
+    let mut pass: String = String::new();
+    io::stdin().read_line(&mut pass).expect("No Input!");
+    pass = pass.trim().to_string();
+    u_details.pass = &pass;
+}
+
 fn main() {
     let _init = App::new("Dev-Upload!")
     .version("0.1.0")
