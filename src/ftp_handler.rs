148f01ad389826ed6db1f8c9dc268938a625e472
dev-harsh1998 <dev-harsh1998@hotmail.com>
2019-08-16 14:04:10 +0530

    devupload: show name and file size before uploading
    * To the nearest mb
    
    Signed-off-by: dev-harsh1998 <dev-harsh1998@hotmail.com>

--- src/ftp_handler.rs
+++ src/ftp_handler.rs
@@ -3,6 +3,7 @@ use ftp::FtpStream;
 use ftp::types::FileType;
 use std::path::Path;
 use std::fs::File;
+use std::fs::metadata;
 mod credential_setup;
 
 pub fn upload_file(_choice: u8, _file_path: String) {
@@ -20,6 +21,9 @@ pub fn upload_file(_choice: u8, _file_path: String) {
     let _file_name = Path::new(& _file_path).file_name()
         .unwrap().to_str().unwrap();
     let mut file_stream = File::open(&_file_path).unwrap();
+    // Compute File Size
+    let mut _file_size = metadata(& _file_path);
+    let _file_size:f64 =  (_file_size.unwrap().len()) as f64 / 1000000 as f64;
     //
     // FTP begin
     //
@@ -30,6 +34,8 @@ pub fn upload_file(_choice: u8, _file_path: String) {
         .unwrap_or_else(|err|panic!("Can't login here's the response: {}", err));
     _ftp_stream.transfer_type(FileType::Binary)
         .expect("Can't set to binary upload mode");
+    // Display File size
+    println!("Starting to upload file: {} , with size: {} Megabytes", _file_name, _file_size);
     let _success = _ftp_stream.put(_file_name, &mut file_stream);
     let _success = match _success {
         Ok(_) => {
