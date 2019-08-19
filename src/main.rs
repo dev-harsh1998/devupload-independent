69cca84b7eba9bd1d28c3e3a85930493f04aca5c
dev-harsh1998 <dev-harsh1998@hotmail.com>
2019-08-19 14:36:08 +0530

    devupload: Version 1.1.0 it is.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod ftp_handler;
 
 fn main() {
     let _init = App::new("Dev-Upload!")
-        .version("1.0.0")
+        .version("1.1.0")
         .author("dev-harsh1998 <dev-harsh1998@hotmail.com>")
         .about("A Handy tool for Android developer community to ease file upload to ftp clients.")
         .arg(
